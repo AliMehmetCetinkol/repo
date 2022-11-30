fee2e531ee62159b43e6e1d3165450bb0c9b2a32
mehmet cetinkol <alimehmet.cetinkol@gmail.com>
2022-11-30 21:32:32 +0300

    dfsdfs

--- javade.java
+++ javade.java
@@ -1,10 +1,29 @@
 class javade{
     public static void main(String[] args) {
-        System.out.println("denedim oldu...");
+        System.out.println("Hello, World");    
+        Sample.foo();
+        Sample.bar();
+        Mample.tar();
+        System.out.println("Goodbye, World");
     }
 }
-class mample{
-    public static void foo(){
-        System.out.println("asd eklendi...");
+class Sample{
+    public static void foo()
+    {
+        System.out.println("Sample.foo");
+        Mample.tar();
+    }
+
+    public static void bar()
+    {
+        System.out.println("Sample.bar");
+    }
+}
+
+
+class Mample {
+    public static void tar()
+    {
+        System.out.println("Mample.tar");
     }
 }
