b62206ed6ba7a7a36566391a382aa3d2241892c0
mehmet cetinkol <alimehmet.cetinkol@gmail.com>
2022-11-30 21:37:03 +0300

    şimdi de bu

--- javade.java
+++ javade.java
@@ -1,29 +1,11 @@
 class javade{
     public static void main(String[] args) {
-        System.out.println("Hello, World");    
-        Sample.foo();
-        Sample.bar();
-        Mample.tar();
-        System.out.println("Goodbye, World");
+        System.out.println("denedim oldu...");
+        mample.foo();
     }
 }
-class Sample{
-    public static void foo()
-    {
-        System.out.println("Sample.foo");
-        Mample.tar();
-    }
-
-    public static void bar()
-    {
-        System.out.println("Sample.bar");
-    }
-}
-
-
-class Mample {
-    public static void tar()
-    {
-        System.out.println("Mample.tar");
+class mample{
+    public static void foo(){
+        System.out.println("asd eklendi...");
     }
 }
